91d956c3fe8ec03f7d6b944f00b8ab01f3360a6a
DevOma <devosamaupro@gmail.com>
2022-12-04 04:34:25 +0100

    copie branche dev1

---
+++ code/sources/init_struct.c
@@ -0,0 +1,33 @@
+#include "../includes/minishell.h"
+
+void free_struct(t_data *minis)//free le tableau de structure
+{
+    int i;
+
+    i = 0;
+    while(i < minis->nb_cmd)
+    {
+        free(minis->cmd[i].line_cmd);
+        i++;
+    }
+    free(minis->cmd);
+}
+
+
+void init_struct(t_data *minis)//allocation d'un tableau de strcuture et copier le tableau dans chaque structure
+{
+    int i;
+
+    minis->cmd = malloc(sizeof(t_board) * minis->nb_cmd);//allocation d'un tableau de structure
+    if(!minis->cmd)
+        ft_error("Malloc", minis, 2);
+    i = 0;
+    while(i < minis->nb_cmd)//boucle qui met les commandes dans le tableau de structure
+    {
+        minis->cmd[i].line_cmd = ft_strdup(minis->tab_cmd[i]);
+        if(!minis->cmd[i].line_cmd)
+            ft_error("Malloc", minis, 3);
+        i++;
+    }
+    free_tab(minis->tab_cmd, minis->nb_cmd + 1);//free le tableau du split_cmd car les lignes ont ete copiee dans cmd[i]->cmd_line
+}
---
+++ code/sources/main.c
@@ -0,0 +1,35 @@
+#include "../includes/minishell.h"
+
+void control_c()
+{
+	printf("tu as press control c");//il faudra kill les process en cours et free/close
+	exit(1);
+}
+
+void minishell_loop(void)
+{
+	t_data minis;
+
+	signal(SIGINT, &control_c);//si tu appuye sur control c ca quitte
+	while(1)//looop qui lit avec un prompt
+	{
+		minis.line = readline(">$");
+		if(minis.line)
+		{
+			line_to_cmd(&minis);//split les commandes
+			init_struct(&minis);//init struct et put les commandes dans chaque struct
+			
+			//inserer les fonctions ici
+			put_env_var(&minis);//fonction qui substitue la variable env en son contenu dans la ligne de commande
+
+			free_struct((&minis));//free la structure des commandes
+			free(minis.line);//free uniquement si elle existe?
+		}
+	}
+}
+
+int    main(void)
+{
+	minishell_loop();
+	return(1);
+}
