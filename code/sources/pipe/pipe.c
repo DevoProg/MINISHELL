5996cfb9d40c07d52b792d473f7040bff249d250
g <guigui-m@hotmail.com>
2022-12-24 14:35:19 +0100

    pipe v4

--- code/sources/pipe/pipe.c
+++ code/sources/pipe/pipe.c
@@ -9,7 +9,6 @@ void wait_all_pids(t_data *minis)
     {
         waitpid(minis->cmd[i].res_fork, NULL, 0);
         i++;
-
     }
 }
 
@@ -29,13 +28,12 @@ void close_all_pipes(t_data *minis, int **fd)
 void just_one_cmd(t_data *minis, t_board *cmd, char **envp)
 {
     cmd->res_fork = fork();
-    if (cmd->res_fork < 0) {
-        return ;
-    }
+    if (cmd->res_fork < 0)
+        exit(1);//il faudra exit prorpement
     
-    if (cmd->res_fork == 0) {
-        // Child process 1 (ping)
-        if(!ft_is_builtins(minis, cmd))
+    if (cmd->res_fork == 0) 
+    {
+        if (!ft_is_builtins(minis, cmd))
             execve(cmd->cmd_path, cmd->tab, envp);
         ft_check_builtins(minis, cmd);
         exit(1);
@@ -45,12 +43,10 @@ void just_one_cmd(t_data *minis, t_board *cmd, char **envp)
 void first_cmd(t_data *minis, t_board *cmd, char **envp, int **fd, int i)
 {
     cmd->res_fork = fork();
-    if (cmd->res_fork < 0) {
-        return ;
-    }
-    
-    if (cmd->res_fork == 0) {
-        // Child process 1 (ping)
+    if (cmd->res_fork < 0)
+        exit(1);//il faudra quitter prorement
+    if (cmd->res_fork == 0) 
+    {
         dup2(fd[i][1], STDOUT_FILENO);
         close_all_pipes(minis, fd);
         if(!ft_is_builtins(minis, cmd))
@@ -63,12 +59,10 @@ void first_cmd(t_data *minis, t_board *cmd, char **envp, int **fd, int i)
 void middle_cmd(t_data *minis, t_board *cmd, char **envp, int **fd, int i)
 {
     cmd->res_fork = fork();
-    if (cmd->res_fork < 0) {
-        return ;
-    }
-
-    if (cmd->res_fork == 0) {
-        // Child process 2 (grep)
+    if (cmd->res_fork < 0)
+        exit(1);//il faudra quitter prormeent
+    if (cmd->res_fork == 0)
+    {
         dup2(fd[i - 1][0], STDIN_FILENO);
         dup2(fd[i][1], STDOUT_FILENO);
         close_all_pipes(minis, fd);
@@ -82,12 +76,10 @@ void middle_cmd(t_data *minis, t_board *cmd, char **envp, int **fd, int i)
 void last_cmd(t_data *minis, t_board *cmd, char **envp, int **fd, int i)
 {
     cmd->res_fork = fork();
-    if (cmd->res_fork < 0) {
-        return ;
-    }
-
-    if (cmd->res_fork == 0) {
-        // Child process 2 (grep)
+    if (cmd->res_fork < 0) 
+        exit(1);//il faudrda quitter prorpement
+    if (cmd->res_fork == 0)
+    {
         dup2(fd[i - 1][0], STDIN_FILENO);
         close_all_pipes(minis, fd);
         if(!ft_is_builtins(minis, cmd))
@@ -107,7 +99,6 @@ void find_path_struct(t_data *minis)
     while(i < minis->nb_cmd)
     {
         minis->cmd[i].cmd_path = ft_try_path(minis, path, &minis->cmd[i]);
-        //printf("%s\n", minis->cmd[i].cmd_path);
         i++;
     }
 }
@@ -142,12 +133,16 @@ void ft_pipe(t_data *minis, char **envp)
         return;
     }
     fd = malloc(sizeof(int*) * (minis->nb_cmd - 1));
+    if(!fd)
+        exit(1);//il faudra quitter proprement
     i = 0;
     while(i < minis->nb_cmd - 1)
     {
         fd[i] = malloc(sizeof(int) * 2);
+        if(!fd[i])
+            exit(1);//il faudra quitter prorpement
         if (pipe(fd[i]) == -1)
-            return;
+            exit(1);//il faudra quitter prorpement
         i++;
     }
     ft_execute(minis, fd, envp);
