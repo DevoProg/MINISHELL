26572d114f7ca7ae428dc13c1082b934bf3d0f67
g <guigui-m@hotmail.com>
2022-12-24 14:47:23 +0100

    pipe6

--- code/sources/pipe/pipe.c
+++ code/sources/pipe/pipe.c
@@ -98,7 +98,14 @@ void find_path_struct(t_data *minis)
     path = list_chr(minis->env, "PATH");
     while(i < minis->nb_cmd)
     {
-        minis->cmd[i].cmd_path = ft_try_path(minis, path, &minis->cmd[i]);
+        if(!ft_is_builtins(minis, &minis->cmd[i]))
+        {
+
+            minis->cmd[i].cmd_path = ft_try_path(minis, path, &minis->cmd[i]);
+            if(!minis->cmd[i].cmd_path)
+                printf("command not found: %s\n", minis->cmd[i].tab[0]);
+        }
+
         i++;
     }
 }
